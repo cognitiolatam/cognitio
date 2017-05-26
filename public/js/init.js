f96aa2ee97c1a4da9b35211d3082ca3f0ca6dcc3
IvanaLei <ivanam@xyclon.com.ar>
2017-05-25 22:23:51 -0300

    Maquetacion Home

--- public/js/init.js
+++ public/js/init.js
@@ -1,5 +1,39 @@
 $(document).ready(function() {
 
+    /**init functions**/
+    headerSize();
+    adminContent();
+
+    $(window).resize(function() {
+        headerSize();
+      });
+
+    function headerSize(){
+        var windowWidth = $(window).width();
+        var windowHeight = $(window).height();
+        if (windowWidth>=992){
+            $( "#header" ).css({
+                "height": "600px"
+            });
+        }else if (windowWidth>769 && windowWidth<992){
+            $( "#header" ).css({
+                "height": "500px"
+            });
+
+        }else if (windowWidth>600 && windowWidth<=768){
+            $( "#header" ).css({
+                "height": "480px"
+            });
+
+        }else if (windowWidth<=600){
+            $( "#header" ).css({
+                "height": "400px"
+            });
+        };
+        var hslider =  $( "#header" ).css('height');
+        $('.center').css('height', hslider);
+    }
+
     $("#owl-demo").owlCarousel({
         navigation: true,
         navigationText: [
@@ -8,31 +42,21 @@ $(document).ready(function() {
         ]
     });
 
-    $( window ).scroll(function() {
-        var height = $(window).scrollTop();
-
-        if(height  > 80) {
-            $("body").removeClass( "menu-transparent" );
-            $(".logo-b-img").hide();
-        }else{
-            $("body").addClass( "menu-transparent" );
-            $(".logo-b-img").show();
-        }
-    });
-
-    $('#settings-content').hide();
-    $('#calendar-content').show();
-
-    $('#calendario').click(function(e){
-        e.preventDefault();
+    function adminContent(){
         $('#settings-content').hide();
         $('#calendar-content').show();
-    })
 
-    $('#settings').click(function(e){
-        e.preventDefault();
-        $('#settings-content').show();
-        $('#calendar-content').hide();
-    })
+        $('#calendario').click(function(e){
+            e.preventDefault();
+            $('#settings-content').hide();
+            $('#calendar-content').show();
+        })
+
+        $('#settings').click(function(e){
+            e.preventDefault();
+            $('#settings-content').show();
+            $('#calendar-content').hide();
+        });
+    };
 
 });
