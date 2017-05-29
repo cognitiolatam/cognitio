a74dc79b11e45a1b02897116af53e315c85013e8
IvanaLei <ivanam@xyclon.com.ar>
2017-05-29 17:44:46 -0300

    Maquetacion Vistas

--- public/js/init.js
+++ public/js/init.js
@@ -3,6 +3,7 @@ $(document).ready(function() {
     /**init functions**/
     headerSize();
     adminContent();
+    smoothScroll();
 
     $(window).resize(function() {
         headerSize();
@@ -34,15 +35,59 @@ $(document).ready(function() {
         $('.center').css('height', hslider);
     }
 
-    $("#owl-demo").owlCarousel({
-        navigation: true,
-        navigationText: [
-            "<span class='glyphicon glyphicon-chevron-left'></span>",
-            "<span class='glyphicon glyphicon-chevron-right'></span>"
-        ],
-        itemsTablet:[992,3],
-        autoPlay: true
+    $( '.modal-ingresar' ).on( 'click', function(e) {
+        e.preventDefault();
+        var modal_add_content = bootbox.dialog({
+            message: "<form><div class='form-group'><label>Email</label><input type='email' class='form-control'></div><div class='form-group'><label>Password</label><input type='password' class='form-control'></div></form>",
+            buttons: {
+                ok: {
+                    label: 'Ingresar',
+                    className: 'btn-enviar',
+                    callback: function(e) {
+                        window.location="admin-profesor.html";
+                    }
+                }
+            }
+        });
+        modal_add_content.addClass("modal-add");
     });
+
+    function smoothScroll(){
+        $('a[href*="#"]')
+        .not('[href="#"]')
+        .not('[href="#0"]')
+        .click(function(event) {
+            // On-page links
+            if (
+                location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '')
+                &&
+                location.hostname == this.hostname
+            ) {
+                // Figure out element to scroll to
+                var target = $(this.hash);
+                target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+                // Does a scroll target exist?
+                if (target.length) {
+                    // Only prevent default if animation is actually gonna happen
+                    event.preventDefault();
+                    $('html, body').animate({
+                        scrollTop: target.offset().top - 70
+                    }, 1000, function() {
+                        // Callback after animation
+                        // Must change focus!
+                        var $target = $(target);
+                        $target.focus();
+                        if ($target.is(":focus")) { // Checking if the target was focused
+                            return false;
+                        } else {
+                            $target.attr('tabindex','-1'); // Adding tabindex for elements not focusable
+                            $target.focus(); // Set focus again
+                        };
+                    });
+                }
+            }
+        });
+    }
 
     function adminContent(){
         $('#settings-content').hide();
