a0690ac160a9c5b300519ad1223dc638ef41b5f8
IvanaLei <ivanam@xyclon.com.ar>
2017-07-10 17:37:11 -0300

    Maquetacion Vistas

--- public/js/init.js
+++ public/js/init.js
@@ -2,8 +2,8 @@ $(document).ready(function() {
 
     /**init functions**/
     headerSize();
-    smoothScroll();
-    changeViewProfesor();
+    // smoothScroll();
+    // changeViewProfesor();
 
     $(window).resize(function() {
         headerSize();
@@ -40,55 +40,41 @@ $(document).ready(function() {
         $('.center').css('height', hslider);
     }
 
-    function smoothScroll(){
-        $('a[href*="#"]')
-        .not('[href="#"]')
-        .not('[href="#0"]')
-        .click(function(event) {
-            // On-page links
-            if (
-                location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '')
-                &&
-                location.hostname == this.hostname
-            ) {
-                // Figure out element to scroll to
-                var target = $(this.hash);
-                target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
-                // Does a scroll target exist?
-                if (target.length) {
-                    // Only prevent default if animation is actually gonna happen
-                    event.preventDefault();
-                    $('html, body').animate({
-                        scrollTop: target.offset().top - 70
-                    }, 1000, function() {
-                        // Callback after animation
-                        // Must change focus!
-                        var $target = $(target);
-                        $target.focus();
-                        if ($target.is(":focus")) { // Checking if the target was focused
-                            return false;
-                        } else {
-                            $target.attr('tabindex','-1'); // Adding tabindex for elements not focusable
-                            $target.focus(); // Set focus again
-                        };
-                    });
-                }
-            }
-        });
-    }
-
-    function changeViewProfesor(){
-        $('.view-detail').click(function(e){
-            e.preventDefault();
-            $('#professorsList').addClass('detail-view');
-            $('#professorsList').removeClass('block-view');
-        })
-
-        $('.view-block').click(function(e){
-            e.preventDefault();
-            $('#professorsList').addClass('block-view');
-            $('#professorsList').removeClass('detail-view');
-        })
-    }
+    // function smoothScroll(){
+    //     $('a[href*="#"]')
+    //     .not('[href="#"]')
+    //     .not('[href="#0"]')
+    //     .click(function(event) {
+    //         // On-page links
+    //         if (
+    //             location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '')
+    //             &&
+    //             location.hostname == this.hostname
+    //         ) {
+    //             // Figure out element to scroll to
+    //             var target = $(this.hash);
+    //             target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+    //             // Does a scroll target exist?
+    //             if (target.length) {
+    //                 // Only prevent default if animation is actually gonna happen
+    //                 event.preventDefault();
+    //                 $('html, body').animate({
+    //                     scrollTop: target.offset().top - 70
+    //                 }, 1000, function() {
+    //                     // Callback after animation
+    //                     // Must change focus!
+    //                     var $target = $(target);
+    //                     $target.focus();
+    //                     if ($target.is(":focus")) { // Checking if the target was focused
+    //                         return false;
+    //                     } else {
+    //                         $target.attr('tabindex','-1'); // Adding tabindex for elements not focusable
+    //                         $target.focus(); // Set focus again
+    //                     };
+    //                 });
+    //             }
+    //         }
+    //     });
+    // }
 
 });
